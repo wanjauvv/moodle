a8375b7ad06b75d33fb2c7a05ddd28b16a13cd24
wanjauvv <wanjauv@gmail.com>
2020-08-16 09:19:06 +0000

    third  commit

---
+++ e-learning-primary/config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodledb3';
+$CFG->dbuser    = 'postgres';
+$CFG->dbpass    = 'moodle';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://3.22.231.183/e-learning-primary';
+$CFG->dataroot  = '/var/www/moodledata-elearning2';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
---
+++ e-learning-secondary/config.php
@@ -0,0 +1,28 @@
+<?php  // Moodle configuration file
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodledb2';
+$CFG->dbuser    = 'postgres';
+$CFG->dbpass    = 'moodle';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://3.22.231.183/e-learning-secondary';
+$CFG->dataroot  = '/var/www/moodledata-elearning';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
